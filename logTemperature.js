0bc2aba636d43fffee6b591a68d60a32c4ed78a6
petre2dor <petre.2dor@gmail.com>
2016-03-27 20:41:13 +0200

    a bit of refactoring and better logging

--- logTemperature.js
+++ logTemperature.js
@@ -1,11 +1,7 @@
 var fs = require('fs');
-fs.appendFileSync('log/error/2016.log', 'start');
 var config = require('./config.json');
-// read temperature file
-var temperature_inside = fs.readFileSync('sampledata', 'utf8');
-var temperature_outside = fs.readFileSync('sampledata1', 'utf8');
-var errorSentToday = fs.readFileSync('errorSentToday.flag', 'utf8');
 
+//getting ready some date variables
 var now = new Date();
 var unix_timestamp = Date.now(); // in milliseconds
 var date_and_time = now.getFullYear() + '-' +
@@ -17,45 +13,65 @@ var date_and_time = now.getFullYear() + '-' +
 var year = now.getFullYear() + '.csv';
 var month = now.getFullYear() + '-' + (now.getMonth() + 1) + '.csv';
 var day = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + '.csv';
-var flag = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
 
-var email   = require("./node_modules/emailjs/email");
-var server  = email.server.connect({
-   user:    config.email.user,
-   password:config.email.pass,
-   host:    config.email.host,
-   ssl:     true
-});
-
-
+// temperature inside
 try {
+    // read temperature file
+    var temperature_inside = fs.readFileSync('sampledata/inside', 'utf8');
     // if there is a valid temperature to read
     if (temperature_inside.indexOf('YES') < 0) {
         throw 'Temperature inside invalid!';
     }
     var array = temperature_inside.split('=');
-    var temperature = parseInt(array.pop())/1000;
-    var inside = temperature;
+    var inside = parseInt(array.pop())/1000;
+} catch (e) {
+    // log error
+    console.log(e);
+    fs.appendFileSync('log/error/'+now.getFullYear()+'.log',
+        date_and_time + ': \n' + e + "\n"
+    );
+    sendErrorEmail(config, date_and_time, e);
+    var inside = '';
+}
+
+// temperature outside
+try {
+    // read temperature file
+    var temperature_outside = fs.readFileSync('sampledata/outside', 'utf8');
 
     if (temperature_outside.indexOf('YES') < 0) {
         throw 'Temperature outside invalid!';
     }
-    array = temperature_outside.split('=');
-    temperature = parseInt(array.pop())/1000;
-    var outside = temperature;
-    str = date_and_time  + ',' + inside + ',' + outside + '\n';
-
-    fs.appendFileSync('log/temperature/' + year, str);
-    fs.appendFileSync('log/temperature/' + month, str);
-    fs.appendFileSync('log/temperature/' + day, str);
+    var array = temperature_outside.split('=');
+    var outside = parseInt(array.pop())/1000;
 } catch (e) {
-    // log temp data
+    // log error
+    console.log(e);
     fs.appendFileSync('log/error/'+now.getFullYear()+'.log',
-        date_and_time + ': \n' +
-        e + "\n" +
-        temperature_inside + "\n" +
-        temperature_outside + "\n"
+        date_and_time + ': \n' + e + "\n"
     );
+    sendErrorEmail(config, date_and_time, e);
+    var outside = '';
+}
+
+var str = date_and_time  + ',' + inside + ',' + outside + '\n';
+fs.appendFileSync('log/temperature/' + year, str);
+fs.appendFileSync('log/temperature/' + month, str);
+fs.appendFileSync('log/temperature/' + day, str);
+
+
+function sendErrorEmail(config, date_and_time, message){
+    var email   = require("./node_modules/emailjs/email");
+    var server  = email.server.connect({
+       user:    config.email.user,
+       password:config.email.pass,
+       host:    config.email.host,
+       ssl:     true
+    });
+
+    var errorSentToday = fs.readFileSync('errorSentToday.flag', 'utf8');
+    var now = new Date();
+    var flag = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
 
     if(errorSentToday != flag){
         //update flag
@@ -63,13 +79,14 @@ try {
 
         // send email error
         server.send({
-            text:            'Something went wrong on TempPi: ' + temperature_inside,
+            text:            'Something went wrong on TempPi: ' + message,
             from:            config.email.from,
             to:              config.email.to,
             subject:         'Error on TempPi',
             'In-Reply-To':   config.email.inReplyToErr,
             References:      config.email.inReplyToErr
         }, function(err, message) {
+            console.log(err);
             if(err){
                 // log err
                 fs.appendFileSync('log/error/'+now.getFullYear()+'.log' , date_and_time + ': email error - ' + err + "\n");
